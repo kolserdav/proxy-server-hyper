5d775439bdd82092385828afd830b04a9efaab1e
kolserdav <serega12101983@gmail.com>
2023-02-08 19:32:59 +0700

    save hyper proxy example

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,54 @@
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Client, Request, Response, Server, Uri};
+use hyper::{Body, Client, Request, Response, Server, Uri, body::HttpBody};
 pub mod error;
 pub mod prelude;
 use prelude::*;
 use std::convert::Infallible;
 use std::net::SocketAddr;
 
+use hyper::client::HttpConnector;
+use futures::{TryFutureExt, TryStreamExt};
+use hyper_proxy::{Proxy, ProxyConnector, Intercept};
+use headers::Authorization;
+use std::error::Error;
+use tokio::io::{stdout, AsyncWriteExt as _};
+
 #[tokio::main]
-pub async fn proxy() {
-    let config = create_config().expect("Failed parse config");
-    println!(
-        "Listen proxy server at: http://{:?}:{} ...",
-        &config.host, &config.port
-    );
-    let addr = SocketAddr::from((config.host, config.port));
-    let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(target)) });
+pub async fn proxy() -> Result<(), Box<dyn Error>> {
+    let proxy = {
+        let proxy_uri = "http://127.0.0.1:3000".parse().unwrap();
+        let mut proxy = Proxy::new(Intercept::All, proxy_uri);
+        proxy.set_authorization(Authorization::basic("John Doe", "Agent1234"));
+        let connector = HttpConnector::new();
+        let proxy_connector = ProxyConnector::from_proxy(connector, proxy).unwrap();
+        proxy_connector
+    };
 
-    let server = Server::bind(&addr).serve(make_svc);
+    // Connecting to http will trigger regular GETs and POSTs.
+    // We need to manually append the relevant headers to the request
+    let uri: Uri = "http://127.0.0.1:3001".parse().unwrap();
+    let mut req = Request::get(uri.clone()).body(hyper::Body::empty()).unwrap();
 
-    if let Err(e) = server.await {
-        eprintln!("server error: {}", e);
+    if let Some(headers) = proxy.http_headers(&uri) {
+        req.headers_mut().extend(headers.clone().into_iter());
     }
+
+    let client = Client::builder().build(proxy);
+    let mut resp = client.request(req).await?;
+    println!("Response: {}", resp.status());
+    while let Some(chunk) = resp.body_mut().data().await {
+        stdout().write_all(&chunk?).await?;
+    }
+
+    // Connecting to an https uri is straightforward (uses 'CONNECT' method underneath)
+    let uri = "https://my-remote-websitei-secured.com".parse().unwrap();
+    let mut resp = client.get(uri).await?;
+    println!("Response: {}", resp.status());
+    while let Some(chunk) = resp.body_mut().data().await {
+        stdout().write_all(&chunk?).await?;
+    }
+
+    Ok(())
 }
 
 async fn target(_req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
